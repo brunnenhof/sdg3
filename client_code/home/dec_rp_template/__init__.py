418c66d4419e3a89983058df1690735904e3fdfb
grippe@blue-way.net <grippe@blue-way.net>
2025-04-19 10:47:10 +0000

    Edited home.dec_rp_template

--- client_code/home/dec_rp_template/__init__.py
+++ client_code/home/dec_rp_template/__init__.py
@@ -11,6 +11,9 @@ class dec_rp_template(dec_rp_templateTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
-    self.dec_rp_expl.text = self.item['dexpl']
-    self.dec_rp_value.text = self.item['dvalue']
+    self.pol_expl.text = self.item['pol_expl']
+    self.pol_name.text = self.item['pol_name']
+    self.slide_min.text = self.item['pol_tltl']
+    self.slide_max.text = self.item['pol_gl']
+    self.slide_val.text = self.item['slide_val']
 
