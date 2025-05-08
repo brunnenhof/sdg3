ab6a6da1cd876b00e180abff0df5fb5b72a3f834
grippe@blue-way.net <grippe@blue-way.net>
2025-05-08 17:39:40 +0000

    Edited home and mg

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -136,7 +136,85 @@ class home(homeTemplate):
     self.gm_card_wait_1_temp_title.text = mg.gm_card_wait_1_temp_title_tx
     self.credits.text = mg.credits_btn_tx
 
+  def set_lang(self, my_loc):
+    if my_loc == 2:
+      self.top_btn_help.text = mg.top_btn_help_de_sie
+      self.top_btn_thanks.text = mg.top_btn_thanks
+      self.top_btn_poc.text = 'PoC'
+    self.top_join_game.text = mg.top_join_game
+    self.top_start_game.text = mg.top_start_game
+    self.p_lb_choose_game.text = mg.p_lb_choose_game
+    self.p_btn_select_game.text = mg.p_btn_select_game
+    self.gm_board.text = mg.msg_gm_board
+    self.gm_board_info.content = mg.msg_gm_board_info
+    self.cb_af.text = mg.cb_af_tx
+    self.cb_us.text = mg.cb_us_tx
+    self.cb_cn.text = mg.cb_cn_tx
+    self.cb_me.text = mg.cb_me_tx
+    self.cb_sa.text = mg.cb_sa_tx
+    self.cb_la.text = mg.cb_la_tx
+    self.cb_pa.text = mg.cb_pa_tx
+    self.cb_ec.text = mg.cb_ec_tx
+    self.cb_eu.text = mg.cb_eu_tx
+    self.cb_se.text = mg.cb_se_tx
+    self.gm_reg_npbp.text = mg.gm_reg_npbp_tx
+    self.gm_card_wait_1_info.content = mg.gm_card_wait_1_info_tx
+    self.gm_card_wait_1_btn_check.text = mg.gm_card_wait_1_btn_check_tx
+    self.gm_start_round.text = mg.gm_card_wait_1_btn_kick_off_round_1_tx
+    self.setup_npbp_label.text = mg.setup_npbp_label_tx
+    self.pcr_rb_af.text = mg.cb_af_tx
+    self.pcr_rb_us.text = mg.cb_us_tx
+    self.pcr_rb_cn.text = mg.cb_cn_tx
+    self.pcr_rb_me.text = mg.cb_me_tx
+    self.pcr_rb_sa.text = mg.cb_sa_tx
+    self.pcr_rb_la.text = mg.cb_la_tx
+    self.pcr_rb_pa.text = mg.cb_pa_tx
+    self.pcr_rb_ec.text = mg.cb_ec_tx
+    self.pcr_rb_eu.text = mg.cb_eu_tx
+    self.pcr_rb_se.text = mg.cb_se_tx
 
+    self.pcr_rb_pov.text = mg.cb_pov_tx
+    self.pcr_rb_ineq.text = mg.cb_ineq_tx
+    self.pcr_rb_emp.text = mg.cb_emp_tx
+    self.pcr_rb_food.text = mg.cb_food_tx
+    self.pcr_rb_ener.text = mg.cb_ener_tx
+    self.pcr_rb_fut.text = mg.cb_fut_tx
+
+    self.pcr_title.text = mg.pcr_title_tx
+    self.pcr_col_left_title.text = mg.pcr_col_left_title_tx
+    self.pcr_col_right_title.text = mg.pcr_col_right_title_tx
+    self.pcr_submit.text = mg.pcr_submit_tx
+    self.fut_not_all_logged_in.text = mg.fut_not_all_logged_in_tx
+    #    self.pcr_submit_msg1.text = mg.pcr_submit_msg1
+    #    self.pcr_submit_msg2.text = mg.pcr_submit_msg2
+    self.pcgd_title.text = mg.pcr_title_tx
+    self.pcgd_info_rd1.content = mg.pcgd_rd1_info_tx
+    self.pcgd_generating.text = mg.pcgd_generating_tx
+    self.dec_info.content = mg.dec_info_tx
+    self.dec_title.text = mg.dec_title_tx
+    self.pcgd_advance.text = mg.pcgd_advance_tx
+
+    self.refresh_numbers.text = mg.refresh_numbers_tx
+    self.submit_numbers.text = mg.submit_numbers_tx
+    self.fut_info.content = mg.fut_info_tx 
+    self.fut_bud_lb1.text = mg.fut_bud_lb1_tx 
+    self.fut_bud_lb2.text = mg.fut_bud_lb2_tx 
+    self.fut_but_lb3.text = mg.fut_bud_lb3_tx 
+    self.cpf_lb.text = mg.cfpov_tx
+    self.cpf_lb2.text = mg.cfpov_lb_tx 
+    self.cpf_ineq_lb.text = mg.cfineq_tx
+    self.cpf_ineq_lb2.text = mg.cfineq_lb_tx 
+    self.cpf_emp_lb.text = mg.cfemp_tx
+    self.cpf_emp_lb2.text = mg.cfemp_lb_tx 
+    self.cpf_food_lb.text = mg.cffood_tx
+    self.cpf_food_lb2.text = mg.cffood_lb_tx 
+    self.cpf_ener_lb.text = mg.cfener_tx
+    self.cpf_ener_lb2.text = mg.cfener_lb_tx 
+    self.gm_card_wait_1_temp_title.text = mg.gm_card_wait_1_temp_title_tx
+    self.credits.text = mg.credits_btn_tx
+
+    pass
+    
   def top_btn_thanks_click(self, **event_args):
     alert(content=mg.top_thanks_msg, title=mg.top_thanks_title, large=True)
 
