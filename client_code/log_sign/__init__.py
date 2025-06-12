7aec8da9db39ca9d159f475f69add516325ebe78
grippe@blue-way.net <grippe@blue-way.net>
2025-06-12 09:11:14 +0000

    Edited log_sign

--- client_code/log_sign/__init__.py
+++ client_code/log_sign/__init__.py
@@ -130,3 +130,11 @@ class log_sign(log_signTemplate):
   def login_u_show(self, **event_args):
     self.login_u.focus()
     pass
+
+  def adm_pw_pressed_enter(self, **event_args):
+    """This method is called when the user presses enter in this component."""
+    pass
+
+  def adm_btn_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    pass
